7b08820cddfb7b3b1c09ad31629bc22f64fe6a08
liyasharipova <liyasharipova2107@gmail.com>
2017-10-14 23:31:25 +0300

    addtodb method added

--- src/main/java/com/github/liyasharipova/App.java
+++ src/main/java/com/github/liyasharipova/App.java
@@ -2,6 +2,10 @@ package com.github.liyasharipova;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -11,7 +15,16 @@ import java.util.Set;
  */
 public class App {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    private static Connection conn;
+
+    public App() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+        conn = null;
+        conn = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/mydb", "postgres", "postgres");
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, SQLException {
 
         if (args.length == 0) {
             throw new IllegalArgumentException(
@@ -20,7 +33,8 @@ public class App {
         int k = Integer.parseInt(args[0]);
 
         StringBuilder stringBuilder = new StringBuilder();
-        Set<String> genomParts = null;
+        Set<String> genomParts1 = null;
+        Set<String> genomParts2 = null;
 
         try (Scanner scanner = new Scanner(new File("Genome_1.txt"))) {
             String genom = null;
@@ -28,11 +42,29 @@ public class App {
                 genom = scanner.nextLine();
                 stringBuilder.append(genom);
             }
-            genomParts = extractGenomPartsDividedByFrom(k, stringBuilder);
+            genomParts1 = extractGenomPartsDividedByFrom(k, stringBuilder);
+            addGenomToDB(genomParts1, 1);
+            addGenomToDB(genomParts2, 2);
+
         }
 
 
+    }
 
+
+    private static double getJaccardSimilarity(Set<String> genomParts1, Set<String> genomParts2) {
+
+    }
+
+    private static void addGenomToDB(Set<String> genomParts, int id) throws SQLException {
+        for (String part :
+                genomParts) {
+            PreparedStatement p = conn.prepareStatement("INSERT INTO parts (value, genom_id) " +
+                    "VALUES (?,?)");
+            p.setString(1, part);
+            p.setInt(2, id);
+            p.executeUpdate();
+        }
     }
 
     private static Set<String> extractGenomPartsDividedByFrom(int k, StringBuilder genom) {
@@ -46,4 +78,6 @@ public class App {
         }
         return genomParts;
     }
+
+
 }
